332476f7ed6c355096b99638735c9df41a3498ff
jossoficial <jossoficial78@gmail.com>
2026-08-12 18:40:35 -0600

    Add configuration for API keys and endpoints

---
+++ src/config.py
@@ -0,0 +1,5 @@
+import os
+THESTATS_KEY = os.getenv("THESTATS_API_KEY")
+ISPORTS_KEY = os.getenv("ISPORTS_API_KEY")
+THESTATS_BASE = "https://api.thestatsapi.com/api/football"
+ISPORTS_BASE = "http://api.isportsapi.com"
